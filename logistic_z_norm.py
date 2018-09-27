8901d2f4631a87f18781e1afca6f62e4c390f160
yewwah <yewwah10@gmail.com>
2018-09-28 01:33:07 +0800

    commit

--- logistic_z_norm.py
+++ logistic_z_norm.py
@@ -25,14 +25,15 @@ wt_b = np.zeros((train.shape[1]))
 # Prepare Test Data
 test = load['Xtest']
 test = scaler.transform(test)
+test = np.hstack((test, np.ones((test.shape[0], 1))))
 
 # Load ground truths
 grd_truth_train = load['ytrain'].flatten()
+grd_truth_test = load['ytest'].flatten()
 
 
 def sigmoid(weights, train):
     x = weights.dot(train.T)
-    # import pdb;pdb.set_trace()
     return 1 / (1 + np.nan_to_num(np.exp(-x)))
 
 
@@ -65,13 +66,52 @@ def check_convergence(beta_old, beta_new, tol, iters):
     coef_change = np.abs(beta_old - beta_new)
     return not (np.any(coef_change > tol) and iters < max_iters)
 
+def compute_accuracy(grd_truth, predictions):
+    # Assign class 0 or class 1
+    pred = [0 if x < 0.5 else 1 for x in predictions]
+    pred = zip(grd_truth, pred)
+    acc = [1 if int(x[0]) == x[1] else 0 for x in pred]
+    return sum(acc)/float(len(grd_truth))
 
-weights_old = np.ones(wt_b.shape)
-iter = 0
-while not check_convergence(weights_old, wt_b, 1e-5, iter):
-    weights_old = wt_b
-    pred = sigmoid(wt_b, train)
-    first_der = diff_first(train, pred, grd_truth_train, 1.0, wt_b)
-    hessian_matrix = hessian(train, pred, 1.0)
-    wt_b = newton_update(wt_b, first_der, hessian_matrix)
-    iter += 1
+# Prepare regularization
+regularizers = []
+lam = 1
+while lam < 100:
+    if lam < 10:
+        regularizers.append(lam)
+        lam += 1
+    else:
+        regularizers.append(lam+5)
+        lam += 5
+
+train_errors = []
+test_errors = []
+for i in regularizers:
+    weights_old = np.ones(wt_b.shape)
+    wt_b = np.zeros((train.shape[1]))
+    iter = 0
+    while not check_convergence(weights_old, wt_b, 1e-5, iter):
+        weights_old = wt_b
+        pred = sigmoid(wt_b, train)
+        first_der = diff_first(train, pred, grd_truth_train, i, wt_b)
+        hessian_matrix = hessian(train, pred, i)
+        wt_b = newton_update(wt_b, first_der, hessian_matrix)
+        iter += 1
+
+    detection = sigmoid(wt_b, train)
+    train_errors.append(1.0 - compute_accuracy(grd_truth_train, detection))
+
+    detection = sigmoid(wt_b, test)
+    test_errors.append(1.0 - compute_accuracy(grd_truth_test, detection))
+
+x = regularizers
+y_test = test_errors
+y_train = train_errors
+fig, ax = plt.subplots()
+ax.plot(x, y_train, 'g', label='Train Error')
+ax.plot(x, y_test, 'r', label='Test Error')
+leg = ax.legend()
+
+plt.title = 'Lambda against Error'
+plt.savefig('logistic_norm.png')
+plt.show()
